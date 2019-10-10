89af641f21924e442083b5adf4e8fd8e06e057a0
Jerry M <developer.jerrymannel@gmail.com>
2019-10-10 22:49:24 +0530

    dockerfiles for 1.10 1.11 1.13

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,2 +1,10 @@
 docker build -t jerrymannel/gomongotest:1.13 .
-docker tag jerrymannel/gomongotest:1.13 jerrymannel/gomongotest
+
+# building for go 1.10
+docker build -t jerrymannel/gomongotest:1.10 -f Dockerfile_go1.10 .
+
+# building for go 1.11
+docker build -t jerrymannel/gomongotest:1.11 -f Dockerfile_go1.11 .
+
+# building for go 1.13
+docker build -t jerrymannel/gomongotest:1.13 -f Dockerfile_go1.13 .
