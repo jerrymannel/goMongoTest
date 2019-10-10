e3af25b3fdd6021858ea7a2c71440746e9ac4e75
Jerry M <developer.jerrymannel@gmail.com>
2019-10-10 15:22:09 +0530

    docker build file

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o goMongoTest-x86 .
 env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o goMongoTest-macos .
 env GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o goMongoTest-win32.exe .
-
-docker build -t jerrymannel/gomongotest:1 .
-dim tag jerrymannel/gomongotest:1 jerrymannel/gomongotest
---
+++ buildDocker.sh
@@ -0,0 +1,2 @@
+docker build -t jerrymannel/gomongotest:1 .
+dim tag jerrymannel/gomongotest:1 jerrymannel/gomongotest
