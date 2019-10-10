56c2a9ceecec6b8d5f3d4c410627aa7342301139
Jerry M <developer.jerrymannel@gmail.com>
2019-10-10 16:47:38 +0530

    built images using go 1.13

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,2 +1,2 @@
-docker build -t jerrymannel/gomongotest:1 .
-dim tag jerrymannel/gomongotest:1 jerrymannel/gomongotest
+docker build -t jerrymannel/gomongotest:1.13 .
+docker tag jerrymannel/gomongotest:1.13 jerrymannel/gomongotest
