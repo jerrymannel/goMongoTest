1ff52ae062221c7e799b7c0b236d6a6fff09dd0b
Jerry M <developer.jerrymannel@gmail.com>
2021-07-13 01:09:03 +0530

    some more tesys

--- testCursor.go
+++ testCursor.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -13,13 +14,25 @@ import (
 
 // Test random comment so that VSCode shuts up
 type Execution struct {
-	ID      string `bson:"_id"`
-	GroupID string `bson:"groupId,omitempty"`
-	DealID  string `bson:"dealId,omitempty"`
+	ID                    string         `json:"_id" bson:"_id"`
+	SubInstructionID      string         `json:"subInstructionId,omitempty" bson:"subInstructionId,omitempty"`
+	Priority              int            `json:"priority,omitempty" bson:"priority,omitempty"`
+	Amount                float64        `json:"amount,omitempty" bson:"amount,omitempty"`
+	DealID                string         `json:"dealId" bson:"dealId,omitempty"`
+	OriginallyScheduledOn *CustomDateObj `json:"originallyScheduledOn" bson:"originallyScheduledOn"`
+}
+
+type CustomDateObj struct {
+	Tz     string    `json:"tz,omitempty" bson:"tz,omitempty"`
+	TzInfo string    `json:"tzInfo,omitempty" bson:"tzInfo,omitempty"`
+	Utc    time.Time `json:"utc,omitempty" bson:"utc,omitempty"`
+	Epoch  int64     `json:"epoch,omitempty" bson:"epoch,omitempty"`
 }
 
 func main() {
 	url := "mongodb://localhost:27017"
+
+	dealId := "PT-DEAL-1626018451387-1"
 
 	if os.Getenv("URL") != "" {
 		fmt.Println("Using the URL :: ", os.Getenv("URL"))
@@ -43,15 +56,17 @@ func main() {
 
 	// Pass these options to the Find method
 	findOptions := options.Find()
+	findOptions.SetProjection(bson.D{{"subInstructionId", 1}, {"dealId", 1}})
 
 	// Passing bson.D{{}} as the filter matches all documents in the collection
-	cur, err := collection.Find(context.Background(), bson.D{{"dealId", "PT-DEAL-1625934489122-1"}}, findOptions)
+	cur, err := collection.Find(context.Background(), bson.D{{"dealId", dealId}}, findOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Finding multiple documents returns a cursor
 	// Iterating through the cursor allows us to decode documents one at a time
+	fmt.Println("Inserting into another collection")
 	var elem Execution
 	for cur.Next(context.Background()) {
 		// create a value into which the single document can be decoded
@@ -59,20 +74,19 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		// fmt.Println("\t", elem)
-		insertResult, err := collection2.InsertOne(context.Background(), elem)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("Inserted from exec to ecomm: ", insertResult.InsertedID)
+		// fmt.Println(elem.OriginallyScheduledOn)
+		collection2.InsertOne(context.Background(), elem)
+		fmt.Print("#")
 	}
+	fmt.Println()
 
 	pipeline := bson.D{
-		{"$match", bson.D{{"dealId", "PT-DEAL-1625934489122-1"}}},
+		{"$match", bson.D{{"dealId", dealId}}},
 	}
 
 	aggregateOptions := options.Aggregate()
 
+	fmt.Println("Updating documents in new collection")
 	aggregateCursor, err := collection2.Aggregate(context.Background(), mongo.Pipeline{pipeline}, aggregateOptions)
 	for aggregateCursor.Next(context.Background()) {
 		err := aggregateCursor.Decode(&elem)
@@ -84,9 +98,10 @@ func main() {
 			log.Fatal(err)
 		}
 		if result.MatchedCount != 0 {
-			fmt.Println("Matched and updated an existing document", elem.ID)
+			fmt.Print("#")
 		}
 	}
+	fmt.Println()
 
 	if err := cur.Err(); err != nil {
 		log.Fatal(err)
